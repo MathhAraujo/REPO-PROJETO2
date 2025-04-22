c5964c0d5ab0f1796b8ca4a51c4576097d84f818
Matheus <mhma@cesar.school>
2025-04-22 10:00:09 -0300

    Add uuid for user

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    userId = models.CharField(max_length=32, primary_key=True)
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     userName = models.CharField(max_length=100)
     userEmail = models.EmailField()
     userJoinDate = models.DateField(auto_now_add=True)
