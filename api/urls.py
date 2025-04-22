b2262c0d8f28e0d076f4be9c2284651c7d9e6031
LeoGutzeitt <leonardogbmatos@hotmail.com>
2025-04-22 19:48:07 -0300

    bagunça

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from .views import home_view
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('getAll/', views.get_users, name="get_all_users"),
     path('postUser/', views.postUser, name="post_new_user"),
     path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
 
 
