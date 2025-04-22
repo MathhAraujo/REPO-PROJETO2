26b1b0e168a0e972b5d63b82742bbdb20bae45f8
Davilapeixoto <davilapeixoto52@gmail.com>
2025-04-22 19:55:17 -0300

    Update urls.py

--- api_root/urls.py
+++ api_root/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from api.views import home_view
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('getAll/', views.get_users, name="get_all_users"),
+    path('postUser/', views.postUser, name="post_new_user"),
+    path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path("api/", include("api.urls"), name="api_urls"),
 ]
